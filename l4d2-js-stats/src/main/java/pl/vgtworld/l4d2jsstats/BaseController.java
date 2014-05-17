f572cc972f5abbb239f0c38e6222d1763a4f7731
VGT <tomek@vgtworld.pl>
2014-05-17 14:14:16 +0200

    Move request and response to BaseController. Create method for setting page title. Rename process() method to render() in BaseController.

--- l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/BaseController.java
+++ l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/BaseController.java
@@ -2,12 +2,23 @@ package pl.vgtworld.l4d2jsstats;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
 
 import org.thymeleaf.context.WebContext;
 
 public abstract class BaseController {
 	
-	public String process(String view, HttpServletRequest request, HttpServletResponse response) {
+	@Context
+	protected HttpServletRequest request;
+	
+	@Context
+	protected HttpServletResponse response;
+	
+	protected void setPageTitle(String title) {
+		request.setAttribute("pageTitle", title);
+	}
+	
+	public String render(String view) {
 		WebContext context = new WebContext(request, response, request.getServletContext(), request.getLocale());
 		return AppTemplateEngine.getTemplateEngine().process(view, context);
 	}
