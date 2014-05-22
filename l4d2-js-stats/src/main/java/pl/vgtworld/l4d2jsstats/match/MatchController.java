dde645788e388056da867bb1c3df2677c0ad44d6
VGT <tomek@vgtworld.pl>
2014-05-22 21:15:42 +0200

    Add match type picker as first step in adding match process.

--- l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/match/MatchController.java
+++ l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/match/MatchController.java
@@ -1,41 +1,20 @@
 package pl.vgtworld.l4d2jsstats.match;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import pl.vgtworld.l4d2jsstats.BaseController;
-import pl.vgtworld.l4d2jsstats.difficulty.DifficultyLevelService;
-import pl.vgtworld.l4d2jsstats.difficulty.dto.DifficultyLevelDto;
-import pl.vgtworld.l4d2jsstats.map.GameMapService;
-import pl.vgtworld.l4d2jsstats.map.dto.GameMapDto;
-import pl.vgtworld.l4d2jsstats.user.UserService;
-import pl.vgtworld.l4d2jsstats.user.dto.UserDto;
 
 @Path("/match")
 public class MatchController extends BaseController {
 	
-	@Inject
-	private UserService userService;
-	
-	@Inject
-	private GameMapService mapService;
-	
-	@Inject
-	private DifficultyLevelService difficultyService;
-
 	@GET
-	@Path("/add")
+	@Path("/pick-type")
 	@Produces(MediaType.TEXT_HTML)
-	public String getMatchForm() {
-		UserDto[] activeUsers = userService.findActiveUsers();
-		request.setAttribute("activeUsers", activeUsers);
-		GameMapDto[] maps = mapService.findAll();
-		request.setAttribute("maps", maps);
-		DifficultyLevelDto[] difficultyLevels = difficultyService.findAll();
-		request.setAttribute("difficultyLevels", difficultyLevels);
-		return render("add-match");
+	public String getMatchPicker() {
+		setPageTitle("Pick match type");
+		return render("add-match-picker");
 	}
 }
