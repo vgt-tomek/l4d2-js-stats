51c9f2d601da0e6212eb2e68dbcb91038cb8d1cd
VGT <tomek@vgtworld.pl>
2014-05-24 18:02:36 +0200

    Add form for creating matches.

--- l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/addmatch/MatchController.java
+++ l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/addmatch/MatchController.java
@@ -1,20 +1,35 @@
 package pl.vgtworld.l4d2jsstats.addmatch;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import pl.vgtworld.l4d2jsstats.BaseController;
+import pl.vgtworld.l4d2jsstats.map.GameMapService;
+import pl.vgtworld.l4d2jsstats.map.dto.GameMapDto;
+import pl.vgtworld.l4d2jsstats.match.MatchTypeService;
+import pl.vgtworld.l4d2jsstats.match.dto.MatchTypeDto;
 
 @Path("/match")
 public class MatchController extends BaseController {
 	
+	@Inject
+	private MatchTypeService matchTypeService;
+	
+	@Inject
+	private GameMapService mapService;
+	
 	@GET
-	@Path("/pick-type")
+	@Path("/add")
 	@Produces(MediaType.TEXT_HTML)
 	public String getMatchPicker() {
-		setPageTitle("Pick match type");
-		return render("add-match-picker");
+		setPageTitle("Add match");
+		MatchTypeDto[] matchTypes = matchTypeService.findAll();
+		request.setAttribute("matchTypes", matchTypes);
+		GameMapDto[] maps = mapService.findAll();
+		request.setAttribute("maps", maps);
+		return render("add-match");
 	}
 }
