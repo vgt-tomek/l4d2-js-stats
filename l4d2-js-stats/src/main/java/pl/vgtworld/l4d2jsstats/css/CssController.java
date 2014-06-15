87e1af77af45863efe6e0834b352b702b8510b14
VGT <tomek@vgtworld.pl>
2014-06-15 16:24:24 +0200

    Move css files into /static url.

--- l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/css/CssController.java
+++ l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/css/CssController.java
@@ -12,7 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/css/{filename}")
+@Path("/static/css/{filename}")
 public class CssController {
 	
 	@PathParam("filename")
