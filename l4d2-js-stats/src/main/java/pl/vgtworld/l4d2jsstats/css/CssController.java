0b4923155c5f090ff0d5fa63a3df5b00b8b151d7
VGT <tomek@vgtworld.pl>
2014-06-15 16:20:45 +0200

    Add support for streaming many different css files.

--- l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/css/CssController.java
+++ l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/css/CssController.java
@@ -1,18 +1,42 @@
 package pl.vgtworld.l4d2jsstats.css;
 
 import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/css/style.css")
+@Path("/css/{filename}")
 public class CssController {
+	
+	@PathParam("filename")
+	private String filename;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public InputStream getCssFile() {
-		return CssController.class.getResourceAsStream("/css/style.css");
+	public Response getCssFile() {
+		if (validateFilename(filename)) {
+			InputStream stream = CssController.class.getResourceAsStream("/css/" + filename);
+			if (stream == null) {
+				return Response.status(HttpServletResponse.SC_NOT_FOUND).build();
+			}
+			return Response.ok(stream).build();
+		}
+		return Response.status(HttpServletResponse.SC_NOT_FOUND).build();
+	}
+	
+	private boolean validateFilename(String filename) {
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\-\\.]+\\.css$");
+		Matcher matcher = pattern.matcher(filename);
+		if (matcher.find()) {
+			return true;
+		}
+		return false;
 	}
 }
