48c38c569d43771c4679fccd29ceeddd48eacb9c
VGT <tomek@vgtworld.pl>
2014-06-24 18:26:21 +0200

    Change user's password, when 'change password' form is valid.

--- l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/changepassword/ChangePasswordController.java
+++ l4d2-js-stats/src/main/java/pl/vgtworld/l4d2jsstats/changepassword/ChangePasswordController.java
@@ -8,13 +8,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.annotations.Form;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import pl.vgtworld.l4d2jsstats.BaseController;
 import pl.vgtworld.l4d2jsstats.user.UserService;
+import pl.vgtworld.l4d2jsstats.user.UserServiceException;
 import pl.vgtworld.l4d2jsstats.user.dto.UserDto;
 
 @Path("/change-password")
 public class ChangePasswordController extends BaseController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ChangePasswordController.class);
 	
 	@Inject
 	private UserService userService;
@@ -38,6 +43,17 @@ public class ChangePasswordController extends BaseController {
 		
 		ChangePasswordValidator validator = new ChangePasswordValidator();
 		boolean formValid = validator.validate(form, user.getLogin(), userService);
+		
+		if (formValid) {
+			try {
+				userService.changeUserPassword(user.getId(), form.getNewPassword());
+				return Response.ok(render("change-password-success")).build();
+			} catch (UserServiceException e) {
+				LOGGER.warn("Exception while trying to change user's password ({}).", e.getMessage());
+				request.setAttribute("message", e.getMessage());
+				return Response.ok(render("errors/unexpected-exception")).build();
+			}
+		}
 		
 		request.setAttribute("errors", validator.getErrors());
 		return Response.ok(render("change-password")).build();
